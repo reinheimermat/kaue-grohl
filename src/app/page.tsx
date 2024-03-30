f7791f3d2bcae52a67ff1946512adff0e35101b5
reinheimermat <mreinheimer0@gmail.com>
2024-03-29 23:15:42 -0300

    feat: add about section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import Image from 'next/image'
 import { GridHome } from './components/grid-home'
 import { Sidebar } from './components/sidebar'
+import kaue from '@/assets/kaue.jpg'
 
 export default function Home() {
   return (
@@ -9,6 +11,20 @@ export default function Home() {
       </header>
       <section className="lg:col-span-5">
         <GridHome />
+      </section>
+      <section className="pt-48 lg:col-span-6">
+        <h1 className="text-2xl font-bold uppercase text-primary">Sobre mim</h1>
+
+        <article className="flex flex-col gap-8 pt-16 lg:flex-row">
+          <Image src={kaue} alt="Foto de Kaue Grohl" className="w-full" />
+
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
+            veritatis a distinctio quia? Itaque, impedit rem. Molestias maiores
+            ipsam accusamus ullam similique voluptas alias enim, quidem eveniet
+            mollitia iure dolore.
+          </p>
+        </article>
       </section>
     </main>
   )
